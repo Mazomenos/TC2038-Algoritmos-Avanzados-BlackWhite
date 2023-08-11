f8c664ff60463e4c439b60ad333cb1567d12aca8
mazomenos31 <gustbetancourt@hotmail.com>
2023-08-10 23:32:55 -0700

    added header for activity 1.1 and commented current code

--- Act1_1_MergeSort/main.cpp
+++ Act1_1_MergeSort/main.cpp
@@ -1,3 +1,19 @@
+/*
+Equipo:  
+
+Integrantes:
+
+- Gustavo Betancourt Mazomenos A01252832
+
+- Alejandro (Noogler)
+
+TC2038 
+
+Fecha 10 de Agosto del 2023
+
+
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -5,15 +21,20 @@ using namespace std;
 
 
 int main() {
-    int i,size;
-    cout << "Hello World!";
-    cin >> size;
-    int list[size];
-    for (i = 0; i < size; i++) {
+    //Se declaran las variables del contador y de n (size)
+    int i,n;
+    //Ingresar n (size)
+    cin >> n;
+    int list[n];
+    for (i = 0; i < n; i++) {
         cin >> list[i];
     }
-    for (i = 0; i < size; i++) {
-        cout << list[i];
+    //Print de Valores
+    cout << "Lista de Valores: " << endl;
+    for (i = 0; i < n; i++) {
+        cout << list[i] << endl;
     }
+
+    //return main
     return 0;
 }
