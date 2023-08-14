47549b9a3fe045e29a80e43616e25f570c8ceaef
Alejandro Jauregui Zarate <74626412+Jaza2401@users.noreply.github.com>
2023-08-13 19:22:55 -0700

    Update main.cpp

--- Act1_1_MergeSort_ver2/main.cpp
+++ Act1_1_MergeSort_ver2/main.cpp
@@ -50,6 +50,7 @@ void mergeSort(std::vector<int> &arr, int first, int last) {
   }
 }
 
+// Main time complexity is T(n) = 2T(n/2) + θ(n)
 int main(void) {
   int i, size, flag;
   std::cin >> size;
