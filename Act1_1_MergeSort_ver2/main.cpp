2e266f117b12b2d5ea0eecb7e62becfdc7066cf1
mazomenos31 <gustbetancourt@hotmail.com>
2023-08-20 15:14:06 -0700

    a

--- Act1_1_MergeSort_ver2/main.cpp
+++ Act1_1_MergeSort_ver2/main.cpp
@@ -1,13 +1,38 @@
+/*
+Equipo:  
+
+Integrantes:
+
+- Gustavo Betancourt Mazomenos A01252832
+
+- Alejandro Jauregui Zarate A01252368
+
+TC2038 
+
+Fecha 10 de Agosto del 2023
+
+
+*/
+
+//Imported resources and libraries
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
 
+/*
+This function creates 2 temporary vectors with the divided items and
+then merges them form higher to lower numbers, modifying the main array
+with the correct order
+*/
 void merge(std::vector<int> &arr, int first, int mid, int last) {
   int val1 = mid - first + 1;
   int val2 = last - mid;
 
+
   std::vector<int> leftArr(val1);
   std::vector<int> rightArr(val2);
 
+  
   for (int i = 0; i < val1; i++)
     leftArr[i] = arr[first + i];
   for (int j = 0; j < val2; j++)
@@ -39,6 +64,10 @@ void merge(std::vector<int> &arr, int first, int mid, int last) {
   }
 }
 
+/*
+This function recursively splits the main list in halves and then
+sends them to the merge function
+*/
 void mergeSort(std::vector<int> &arr, int first, int last) {
   if (first < last) {
     int mid = first + (last - first) / 2;
@@ -51,21 +80,15 @@ void mergeSort(std::vector<int> &arr, int first, int last) {
 
 // Main time complexity is T(n) = 2T(n/2) + θ(n)
 int main(void) {
-  int i, size, flag;
+  int i, size;
   std::cin >> size;
   std::cout << "Size: " << size << std::endl;
-  std::cin >> flag;
-  std::cout << "Flag: " << flag << std::endl;
   std::vector<int> myVector(size);
 
-  if (flag) {
+
     for (i = 0; i < size; i++) {
       std::cin >> myVector[i];
-      std::cout << myVector[i] << " ";
     }
-    std::cout << std::endl;
-  }
-  std::cout << std::endl;
 
   mergeSort(myVector, 0, size - 1);
 
