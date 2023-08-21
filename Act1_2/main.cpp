ed03c3568cc7b9840e0094b1b56a9e04ac64b418
mazomenos31 <gustbetancourt@hotmail.com>
2023-08-20 22:14:11 -0700

    implemented greedy algorithm

--- Act1_2/main.cpp
+++ Act1_2/main.cpp
@@ -15,27 +15,61 @@ Fecha 19 de Agosto del 2023
 */
 
 #include <iostream>
-#include <vector>
 
-using namespace std;
 
-int main() {
-  int numDiv, precio, pago, cambio;
 
-  cin>>numDiv;
+//using namespace std;
 
-  vector<int> divisas(numDiv);
+void greedy(int monedas[], int cambio, int n){
+  int temp;
+  for (int i =0; i<n; i++){
 
-  for(int i=0; i<numDiv; i++){
-    cin>>divisas[i];
-    cout<<divisas[i]<<endl;
+    if (monedas[i]>cambio){
+      std::cout << monedas[i] << " ";
+      monedas[i] = 0;
+      std::cout << monedas[i] << std::endl;
+    } 
+    
+    else{
+    std::cout << monedas[i] << " ";
+    temp = cambio/monedas[i];
+    cambio = cambio % monedas[i];
+    monedas[i] = temp;
+    std::cout << monedas[i] << std::endl;
+    }
   }
 
-  cin>>precio>>pago;
+}
 
-  if(precio<pago){
-    cambio = pago-precio;
+
+int main() {
+  int n, p, q, cambio;
+  
+
+  std::cin>>n;
+
+  int monedas[n];
+
+  for(int i=0; i<n; i++){
+    std::cin>>monedas[i];
+  }
+
+  std::cout << "money" << std::endl;
+
+  std::cin>>p>>q;
+
+  
+
+  if(p<q){
+    cambio = q-p;
+
+    std::cout << "cambio: " << cambio << std::endl; 
+
+    greedy(monedas, cambio, n);
+
+
   } else{
-    cout<<"Precio es mayor que el pago"<<endl;
+    std::cout<<"Precio es mayor que el pago"<<std::endl;
+    return 0;
   }
 }
