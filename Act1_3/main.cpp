31bb163baed4113e355079946684ba40880be9d5
mazomenos31 <gustbetancourt@hotmail.com>
2023-08-23 01:18:43 -0700

    Added the basic inputs, created 2d vector (matrix) for labyrinth, added backtraking function with basic 2d vector filled with 0´s for the canvas

--- Act1_3/main.cpp
+++ Act1_3/main.cpp
@@ -1,9 +1,88 @@
+/*
+Equipo:
+
+Integrantes:
+
+- Gustavo Betancourt Mazomenos A01252832
+
+- Alejandro Jauregui Zarate A01252368
+
+TC2038
+
+Fecha 23 de Agosto del 2023
 
 
+*/
+
+//Includes
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <limits>
+
+
+void backtraking(std::vector<std::vector<int>> mat, int m, int n){
+
+    // 2d vector with same dimensions as mat but filled with 0´s
+    std::vector<std::vector<int>> path(m, std::vector<int>(n, 0));
+
+    for (std::vector<int> place : path){
+
+        for (int nums : place){
+            std::cout <<nums << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+
+}
 
 
 int main(){
-    std::cout << "hello world";
+    std::string input;
+    int m, n, num;
+
+
+    std::cin >>m;
+    std::cin >> n;
+
+
+    //removes input buffer to avoid skipping over cin instructions on the first loop rotation
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::vector<std::vector<int>> matrix(m);
+
+    for (int i = 0; i<m; i++){
+        std::cout << i << std::endl;
+        std::getline(std::cin,input);
+        std::istringstream iss(input);
+        while (iss >> num){
+            if (num == 0 or num == 1) {
+                matrix[i].push_back(num);
+            }
+            else {
+                std::cout << "Input contains numbers different from 1 or 0";
+                return -1;
+            }
+        }
+
+    }
+
+    for (std::vector<int> place : matrix){
+
+        for (int nums : place){
+            std::cout <<nums << std::endl;
+        }
+    }
+
+    //backtraking
+    backtraking(matrix, m, n);
+
+    //Ram and pound (maybe?)
+
+
+
+
     return 0;
 }
