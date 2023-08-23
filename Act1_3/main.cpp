620218788defe0fda493e3b8f1556d74cdd6f1ed
mazomenos31 <gustbetancourt@hotmail.com>
2023-08-23 09:51:46 -0700

    implemented backtracking method to solve maze

--- Act1_3/main.cpp
+++ Act1_3/main.cpp
@@ -21,19 +21,43 @@ Fecha 23 de Agosto del 2023
 #include <limits>
 
 
-void backtraking(std::vector<std::vector<int>> mat, int m, int n){
+void backtraking(std::vector<std::vector<int>> mat, std::vector<std::vector<int>> path , int m, int n, int x, int y){
 
     // 2d vector with same dimensions as mat but filled with 0´s
-    std::vector<std::vector<int>> path(m, std::vector<int>(n, 0));
 
-    for (std::vector<int> place : path){
 
-        for (int nums : place){
-            std::cout <<nums << " ";
+
+
+
+    //accept
+    if (x == m-1 and y == n-1){
+        path[x][y] = 1;
+
+
+        for (std::vector<int> place : path){
+
+            for (int nums : place){
+                std::cout <<nums << " ";
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
+        exit(1);
     }
 
+    //deny
+    if (x<0 or y<0 or x>=m or y>=n){
+        return;
+    }
+    if (mat[x][y] == 0){
+        path[x][y] = 0;
+        return;
+    }
+    path[x][y] = 1;
+
+    //loop
+    backtraking(mat, path, m, n, x+1, y); //down
+    backtraking(mat, path , m, n, x, y+1); //right
+    backtraking(mat, path , m, n, x, y-1); //left
 
 
 }
@@ -42,7 +66,7 @@ void backtraking(std::vector<std::vector<int>> mat, int m, int n){
 int main(){
     std::string input;
     int m, n, num;
-
+    int x = 0, y = 0;
 
     std::cin >>m;
     std::cin >> n;
@@ -52,9 +76,10 @@ int main(){
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     std::vector<std::vector<int>> matrix(m);
+    std::vector<std::vector<int>> path(m, std::vector<int>(n, 0));
 
     for (int i = 0; i<m; i++){
-        std::cout << i << std::endl;
+        //std::cout << i << std::endl;
         std::getline(std::cin,input);
         std::istringstream iss(input);
         while (iss >> num){
@@ -68,20 +93,21 @@ int main(){
         }
 
     }
-
+    /*
     for (std::vector<int> place : matrix){
 
         for (int nums : place){
             std::cout <<nums << std::endl;
         }
     }
+     */
 
     //backtraking
-    backtraking(matrix, m, n);
+    backtraking(matrix, path , m, n, x, y);
 
     //Ram and pound (maybe?)
 
-
+    std::cout << "hello";
 
 
     return 0;
