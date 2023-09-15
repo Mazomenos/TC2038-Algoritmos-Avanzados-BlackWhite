d427cd91ef763144135927baebe22222f0af890b
mazomenos31 <gustbetancourt@hotmail.com>
2023-09-15 06:51:46 -0700

    finished palindrome problem

--- Integradora_1/main.cpp
+++ Integradora_1/main.cpp
@@ -151,23 +151,166 @@ void LCSub(vector<vector<char>> transmissions) {
   cout << resultStr << endl;
 }
 
+//COPIA DESDE AQUI
+//--------------------------------------------------------------------------------------------
+
 /*
  * Parte 2: Palindromos mortales
- *
+ * Esta funcion recibe un vector de char y nos imprime el palindromo mas grande que se tiene
+ * dentro del vector.
+ * Tiene una complejidad de tiempo de O(n)
  */
 
-void findMPalindrome(vector<vector<char>> transmissions){
+void palindromoMaligno(vector<char> text)
+{
+
+    //Inicializacion de variables
+    int N = text.size();
+    if (N == 0)
+        return;
+    N = 2 * N + 1; // Position count
+    int L[N]; // LPS Length Array
+    L[0] = 0;
+    L[1] = 1;
+    int C = 1; // centerPosition
+    int R = 2; // centerRightPosition
+    int i; // currentRightPosition
+    int iMirror;  // currentLeftPosition
+    bool expand;
+    int diff;
+    int maxLPSLength = 0;
+    int maxLPSCenterPosition = 0;
+    int start;
+    int end;
+
+    for (i = 2; i < N; i++) {
+
+        //Se calcula el valor simetrico
+        //de la izquierda basado en i
+        iMirror = 2 * C - i;
+
+        expand = false;
+        diff = R - i;
+
+        //Si R es mayor a i
+        //Aplica para las partes del texto ya exploradas
+        if (diff >= 0) {
+
+            /*  Este if usa el valor guardado en el espejo izquierdo de i
+                en caso de que el valor de este sea menor a la diferencia
+                entre R menos i, o en el caso de que se llegue al final del
+                arreglo.
+
+                Este solo guarda el valor simetrico del espejo a su contraparte
+                a la izquierda (dinamico), sin necesidad de expandir.
+
+            */
+            if (L[iMirror] < diff || L[iMirror] == diff && R == N - 1)
+                L[i] = L[iMirror];
+
+            /*  En este if checa si estamos dentro del limite del vector y que el
+                valor espejeado sea igual a la diferencia de R - i.
+                Cuando se da este, usa el valor espejeado para llenar su correspondiente a
+                la derecha y como aun no llega al limite, le dice que puede expandirse.
+
+             */
+            else if (L[iMirror] == diff && R < N - 1) {
+                L[i] = L[iMirror];
+
+                // Expansion required
+                expand = true;
+            }
 
 
-    for (vector<char> transmission : transmissions){
-        for (char c : transmission){
-            cout << c << " ";
+            /*  En este if checa si el valor espejeado es mayor
+                que la diferencia entre R - i, y le asignamos a
+                la posicion i el valor de la diferencia y le
+                decimos que puede expandir
+
+             */
+            else if (L[iMirror] > diff) {
+                L[i] = diff;
+
+                // Expansion required
+                expand = true;
+            }
         }
-        cout << endl;
+
+        //Este else aplica cuando vamos por una parte del texto no explorada
+        else {
+            L[i] = 0;
+
+            // Expansion required
+            expand = true;
+        }
+
+        //Se empieza el proceso de expandir el centro para buscar el largo maximo del palindromo
+        //basado en este
+        if (expand) {
+
+            /*
+             * Se pone una iteracion while la cual busca que se cumplan los siguientes requisitos:
+             *
+             * Que esten dentro de los limites del texto base
+             * Ultima posicion derecha mas larga explorada dentro de N (i + L[i]) < N
+             *
+             * Que el valor simetrico hacia atras sea mayor a 0 (i - L[i]) > 0
+             *
+             * ((i + L[i]) < N && (i - L[i]) > 0)
+             *
+             * and &&
+             *
+             * Ya que se tiene que los limites estan dentro del texto recibido,
+             * se checa primero si es par para darle un 1 directo y luego ya se checa la similitud entre los
+             * lados derecho y izquierdo del centro
+             *
+             * Checa si la posicion es par (i + L[i] + 1) % 2 == 0)
+             *
+             * or ||
+             *
+             * Checa si el char en la posicion derecha es igual al de la izquierda (text[(i + L[i] + 1) / 2] == text[(i - L[i] - 1) / 2]
+             *
+             * ((i + L[i] + 1) % 2 == 0) || (text[(i + L[i] + 1) / 2] == text[(i - L[i] - 1) / 2])
+             */
+
+            while (((i + L[i]) < N && (i - L[i]) > 0)
+                   && (((i + L[i] + 1) % 2 == 0)
+                       || (text[(i + L[i] + 1) / 2]
+                           == text[(i - L[i] - 1) / 2]))) {
+                L[i]++;
+            }
+        }
+
+        // Actualiza valores maximos
+        if (L[i] > maxLPSLength) {
+            maxLPSLength = L[i];
+            maxLPSCenterPosition = i;
+        }
+
+        //En el caso de que la expansion sobrepase
+        //el limite explorado, se recalcula el
+        //centro y el limite explorado
+        if (i + L[i] > R) {
+            C = i;
+            R = i + L[i];
+        }
 
     }
 
+    //Calcula la posicion inicial y la parte a la mitad para q tenga el indice correcto
+    start = (maxLPSCenterPosition - maxLPSLength) / 2;
+
+    end = start + maxLPSLength - 1;
+    
+    cout << start << " " << end << " ";
+
+    for (i = start; i <= end; i++)
+        cout << text[i];
+    cout << endl;
 }
+
+//COPIA HASTA AQUI
+//--------------------------------------------------------------------------------------------
 
 
 int main() {
@@ -191,15 +334,16 @@ int main() {
   vector<char> ta2 = readFile(t2);
   transmissions.push_back(ta2);
 
-  // funciones de la actividad
+  //funciones de la actividad
   //cout << "Parte 1" << endl;
   //analizeTransmission(mcodes, transmissions);
 
   cout << "Parte 2" << endl;
-  findMPalindrome(transmissions);
+    palindromoMaligno(transmissions[0]);
+    palindromoMaligno(transmissions[1]);
 
-  //cout << "Parte 3" << endl;
-  //LCSub(transmissions);
+  cout << "Parte 3" << endl;
+  LCSub(transmissions);
   
   return 0;
 }
