88ba799c8b6a946ed7ba7ef2af36f8df0a7b8c21
Alejandro Jauregui Zarate <74626412+Jaza2401@users.noreply.github.com>
2023-09-28 08:39:29 -0700

    Create Graph.h

---
+++ Act3.2/Graph.h
@@ -0,0 +1,28 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+#include <iostream>
+#include <list>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+class Graph {
+private:
+  int numNodes;
+  int numEdges;
+
+  std::vector<std::list<std::pair<int, int>>> adjList;
+
+  void split(std::string line, std::vector<int> &res);
+
+public:
+  Graph();
+  ~Graph();
+  void readGraph(std::istream &input);
+  void print();
+};
+
+#endif // _GRAPH_H_
